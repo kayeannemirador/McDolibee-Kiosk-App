ff0fe0f22fac48945f9f97c98c17fe92859a022a
Kaye Anne <108706071+engrsashimi@users.noreply.github.com>
2023-04-17 08:52:03 +0800

    importing
    Kiosk V1

---
+++ src/my/kiosk/ComboMeals.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package my.kiosk;
+
+/**
+ *
+ * @author User
+ */
+public class ComboMeals {
+    String comboname;
+    double comboprice; 
+
+    public ComboMeals(String name, double price) {
+        this.comboname = name;
+        this.comboprice = price;
+    }
+
+    public void setData(String name, double price) {
+        this.comboname = name;
+        this.comboprice = price;
+    }
+    
+    
+
+    public String getName() {
+        return comboname;
+    }
+
+    public double getPrice() {
+        return comboprice;
+    }
+    
+    
+    
+    
+}
